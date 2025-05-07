5f62be0aeab5717b3a74dc3def7bf84473460d40
qianlongxu <qianlongxu@gmail.com>
2025-05-07 14:30:53 +0800

    upgrade version to 1.0.1

--- version.sh
+++ version.sh
@@ -2,26 +2,20 @@
 
 set -e
 
-VERSION_NAME=1.0.0
+VERSION_NAME=1.0.1
 VERSION_TARGET=$1
 
 echo "alter version to $VERSION_NAME"
 
 do_version_readme() {
-    
-    cat README.md \
-    | sed "s/\(git checkout -B latest v\)[[:digit:]][[:digit:].]*/\1$VERSION_NAME/g" \
-    | sed "s#\(.*download/k\)\([[:digit:]][[:digit:].]*\)\(/FSPlayer.spec.json\)#\1$VERSION_NAME\3#g" \
-    > README.md.new
-
-    mv -f README.md.new README.md
+    sed -i "" "s#\(.*releases/download/\)\([[:digit:]][[:digit:].]*\)\(/FSPlayer.spec.json\)#\1$VERSION_NAME\3#g" README.md
 }
 
 do_version_xcode() {
-    sed -i "" "s/\([[:space:]]*s.version[[:space:]]*=[[:space:]]*\)\'[[:digit:].]*[[:digit:]]\'/\1\'$VERSION_NAME\'/" FSPlayer.podspec
-    pod install --project-directory=examples/ios >/dev/null
-    pod install --project-directory=examples/macos >/dev/null
-    pod install --project-directory=examples/tvos >/dev/null
+    sed -i "" "s/\([[:space:]]*MARKETING_VERSION:[[:space:]]\)[[:digit:].]*[[:digit:]]/\1$VERSION_NAME/g" FSPlayer.yml
+    ./examples/ios/generate-fsplayer.sh
+    ./examples/tvos/generate-fsplayer.sh
+    ./examples/macos/generate-fsplayer.sh
 }
 
 if [ "$VERSION_TARGET" = "readme" ]; then
