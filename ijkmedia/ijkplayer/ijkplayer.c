95bc2165f14308df22ee3fe9757810ebf715b6e1
qianlongxu <qianlongxu@gmail.com>
2026-01-26 15:51:01 +0800

    fix "msg_queue_get(&mp->ffplayer->msg_queue, msg, block)" BAD_ACCESS crash, ffplayer is NULL

--- ijkmedia/ijkplayer/ijkplayer.c
+++ ijkmedia/ijkplayer/ijkplayer.c
@@ -45,11 +45,11 @@ inline static void ijkmp_destroy(IjkMediaPlayer *mp)
     if (!mp)
         return;
 
-    ffp_destroy_p(&mp->ffplayer);
     if (mp->msg_thread) {
         SDL_WaitThread(mp->msg_thread, NULL);
         mp->msg_thread = NULL;
     }
+    ffp_destroy_p(&mp->ffplayer);
     mp->weak_thiz = NULL;
 
     pthread_mutex_destroy(&mp->mutex);
