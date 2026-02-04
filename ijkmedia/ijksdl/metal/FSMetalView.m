58303f365d2d15866e498c4e535e26222bf28fd3
qianlongxu <qianlongxu@gmail.com>
2026-02-04 16:10:07 +0800

    update dar、scaling、rotate、 color preference refresh picture automatically

--- ijkmedia/ijksdl/metal/FSMetalView.m
+++ ijkmedia/ijksdl/metal/FSMetalView.m
@@ -659,6 +659,7 @@ mp_format * mp_get_metal_format(uint32_t cvpixfmt);
 {
     if (_scalingMode != scalingMode) {
         _scalingMode = scalingMode;
+        [self setNeedsRefreshCurrentPic];
     }
 }
 
@@ -666,6 +667,7 @@ mp_format * mp_get_metal_format(uint32_t cvpixfmt);
 {
     if (_rotatePreference.type != rotatePreference.type || _rotatePreference.degrees != rotatePreference.degrees) {
         _rotatePreference = rotatePreference;
+        [self setNeedsRefreshCurrentPic];
     }
 }
 
@@ -673,6 +675,7 @@ mp_format * mp_get_metal_format(uint32_t cvpixfmt);
 {
     if (_colorPreference.brightness != colorPreference.brightness || _colorPreference.saturation != colorPreference.saturation || _colorPreference.contrast != colorPreference.contrast) {
         _colorPreference = colorPreference;
+        [self setNeedsRefreshCurrentPic];
     }
 }
 
@@ -680,6 +683,7 @@ mp_format * mp_get_metal_format(uint32_t cvpixfmt);
 {
     if (_darPreference.ratio != darPreference.ratio) {
         _darPreference = darPreference;
+        [self setNeedsRefreshCurrentPic];
     }
 }
 
